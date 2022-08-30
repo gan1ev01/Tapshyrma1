4581e9f0cfe3da329521fee47c008c3a337333ca
Administrator <nursultanaibekuulu63@GMAIL.COM>
2022-08-30 00:48:44 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ageCheck(scanner.nextInt());
+        ageCheck(18);
+
+    }
+    static void ageCheck(int age){
+        try {
+            if (age<18){
+                throw new ArithmeticException("Zhashy 18den kichine");
+            }
+            System.out.println(age);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
